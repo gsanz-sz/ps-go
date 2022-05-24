addf729a0ca471ccf7344ac8c1fa164abcac0bb0
Gabriel Sanz Sztochryn <gabrielsanz2106@gmail.com>
2022-05-24 19:39:20 -0300

    Passing by pointers

--- variables/vars.go
+++ variables/vars.go
@@ -9,13 +9,13 @@ func main() {
 	course := "Studying go"
 
 	fmt.Println("Name and course are set to", name, "and", course)
-	updateCourse(course)
+	updateCourse(&course)
 
 	fmt.Println("Name and course now are set to", name, "and", course)
 }
 
-func updateCourse(course string) string {
-	course = "Studying go with GoLang"
-	fmt.Println("Course is set to", course)
-	return course
+func updateCourse(course *string) string {
+	*course = "Studying go with GoLang"
+	fmt.Println("Course is set to", *course)
+	return *course
 }
