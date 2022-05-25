bf88b79b24b0767b39e4dcb94a77a9448447d977
Gabriel Sanz Sztochryn <gabrielsanz2106@gmail.com>
2022-05-25 09:21:41 -0300

    inicializate variable in a conditional

--- conditionals/if.go
+++ conditionals/if.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	dddLengthMins := 275
-	cawLengthMins := 30
+	//dddLengthMins := 275
+	//cawLengthMins := 30
 
-	if dddLengthMins > cawLengthMins {
+	if dddLengthMins, cawLengthMins := 275, 30; dddLengthMins > cawLengthMins {
 		fmt.Println("Docker Deep Dive is longer than Containers on AWS Wavelength.")
+		if dddLengthMins > 240 {
+			fmt.Println("Still longer")
+		}
 	} else if dddLengthMins == cawLengthMins {
 		fmt.Println("Docker Deep Dive and Containers on AWS Wavelength are the same length.")
 	} else {
