b56dbece203717992aebdb894b322b890902f56f
Gabriel Sanz Sztochryn <gabrielsanz2106@gmail.com>
2022-05-25 09:24:03 -0300

    inicializate variable in a conditional

---
+++ conditionals/switch.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	switch "Kubernetes" {
+	case "Docker":
+		fmt.Println("Docker is a containerization engine.")
+	case "Kubernetes":
+		fmt.Println("Kubernetes is a containerization engine.")
+	case "Swarm":
+		fmt.Println("Swarm is a containerization engine.")
+	default:
+		fmt.Println("Unknown.")
+	}
+}
